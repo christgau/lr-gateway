9d38305993fe0138e8c96eeee57062d959b9094f
Steffen Christgau <mail@s14u.de>
2013-09-17 13:01:54 +0200

    feat(http): introduce support for authentication headers

--- src/oris_http.c
+++ src/oris_http.c
@@ -2,6 +2,8 @@
 
 #include <event2/bufferevent_ssl.h>
 #include <openssl/err.h>
+#include <openssl/rand.h>
+#include <openssl/sha.h>
 
 #include "oris_log.h"
 #include "oris_util.h"
@@ -12,10 +14,12 @@
 #pragma warning( disable: 4706 )
 #endif
 
+static const char hex_chars[] = "0123456789abcdef";
+
 static void http_request_done_cb(struct evhttp_request *req, void *ctx);
 static void http_connection_close(struct evhttp_connection *con, void *ctx);
 static const char* oris_get_http_method_string(const enum evhttp_cmd_type method);
-
+static void oris_set_auth_headers(struct evkeyvalq* headers, struct evhttp_uri* uri);
 
 /* taken from libevent https-client sample */
 static void http_request_done_cb(struct evhttp_request *req, void *ctx)
@@ -105,6 +109,49 @@ static const char* oris_get_http_method_string(const enum evhttp_cmd_type method
 	}
 }
 
+static inline void oris_buf_to_hex(const unsigned char* raw, size_t size, char* const s)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++) {
+		s[i] = hex_chars[(raw[i] >> 4) & 0x0f];
+		s[i + 1] = hex_chars[raw[i] & 0x0f];
+	}
+
+	s[size * 2 + 1] = 0;
+}
+
+#define REQ_TOKEN_SIZE SHA256_DIGEST_LENGTH /* 256 bit */
+
+static void oris_set_auth_headers(struct evkeyvalq* headers, struct evhttp_uri* uri)
+{
+	char* userinfo = strdup(evhttp_uri_get_userinfo(uri));
+	char* s = strchr(userinfo, ':');
+	unsigned char token[REQ_TOKEN_SIZE];
+	char token_hex[REQ_TOKEN_SIZE * 2 + 1];
+	SHA256_CTX sha;
+
+	RAND_bytes(token, sizeof(token));
+	oris_buf_to_hex(token, sizeof(token), token_hex);
+
+	*s = 0;
+
+	evhttp_add_header(headers, "OrisUser", userinfo);
+	evhttp_add_header(headers, "OrisRequestToken", token_hex);
+
+	*s = ':';
+
+	SHA256_Init(&sha);
+	SHA256_Update(&sha, userinfo, strlen(userinfo)); /* username:passwordhash */
+	SHA256_Update(&sha, token_hex, sizeof(token_hex)); /* generated nonce aka request token */
+	SHA256_Final(token, &sha);
+
+	oris_buf_to_hex(token, sizeof(token), token_hex);
+	evhttp_add_header(headers, "OrisAuthToken", token_hex);
+
+	free(userinfo);
+}
+
 #define MAX_URL_SIZE 256
 
 void oris_perform_http_on_targets(oris_http_target_t* targets, int target_count,
@@ -136,6 +183,7 @@ void oris_perform_http_on_targets(oris_http_target_t* targets, int target_count,
 			evutil_snprintf(url_buf, MAX_URL_SIZE - 1, "%s%s", evhttp_uri_get_path(
 					targets[i].uri), uri);
 
+			oris_set_auth_headers(output_headers, targets[i].uri);
 			if (method == EVHTTP_REQ_PUT || method == EVHTTP_REQ_POST) {
 				output_buffer = evhttp_request_get_output_buffer(request);
 				evbuffer_add_buffer_reference(output_buffer, body);
