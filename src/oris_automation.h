cc8241c24604a4400796a6f238c5cefe921c9822
Steffen Christgau <mail@s14u.de>
2014-01-17 21:57:10 +0100

    fix(gateway): remove queue solution from automation

--- src/oris_automation.h
+++ src/oris_automation.h
@@ -19,7 +19,8 @@ void oris_automation_trigger(oris_automation_event_t* event, oris_application_in
 void oris_automation_iterate_action(oris_application_info_t* info,
 	const char* request_name, const char* tbl_name);
 
-void oris_automation_request_action(const char* request_name);
+void oris_automation_request_action(oris_application_info_t* info,
+	const char* request_name);
 
 void oris_automation_http_action(oris_application_info_t* info,
     enum evhttp_cmd_type method, pANTLR3_BASE_TREE url,
